47be441566d348daa080cd18f7c4ea692561651c
Lars <lars@stromme.ca>
2013-02-15 08:24:03 +0545

    hatch style changes

--- front-page.php
+++ front-page.php
@@ -58,11 +58,14 @@ get_header();
 </nav>
 
 
-<div class="container">
-	<section class="banner">
-	
-		<div class="span6 banner-photo pull-right">
-			<img class="pull-right" src="<?php echo THEME_IMAGES; ?>temp/banner.jpg">
+<section class="banner">
+	<div class="container">
+		<div class="pull-right banner-photo">
+			<div>
+				<img src="<?php echo THEME_IMAGES; ?>temp/banner.jpg">
+			</div>
+			<img class="photo-shadow" src="<?php echo THEME_IMAGES; ?>backgrounds/960-shadow.png" alt="" />
+			
 		</div>
 		
 		<div class="banner-tag-line">
@@ -72,77 +75,72 @@ get_header();
 				</div>
 			</div>
 		</div>
-		
-		<div class="clearFix">
-			<img src="<?php echo THEME_IMAGES; ?>backgrounds/960-shadow.png" style="width: 100%;"alt="" />
+	</div>
+</section>
+	
+<section class="headliner bumper">
+	<!--<div class="row">
+		<div class="offset1">
+			<h1>Awarded the 2012 Super Service Award</h1>
 		</div>
+	</div>-->
+		<p class="lead pull-right">We're proud to announce that we've been awarded the Angie's List 2012 award for the best window cleaning.</p>
+		<img src="<?php echo THEME_IMAGES; ?>icons/angies-list-super-service-2012-100px.jpg">
 		
-	</section>
 	
-	<section class="headliner bumper">
-		<!--<div class="row">
-			<div class="offset1">
-				<h1>Awarded the 2012 Super Service Award</h1>
-			</div>
-		</div>-->
-			<p class="lead pull-right">We're proud to announce that we've been awarded the Angie's List 2012 award for the best window cleaning.</p>
-			<img src="<?php echo THEME_IMAGES; ?>icons/angies-list-super-service-2012-100px.jpg">
-			
-		
-		
-	</section>
 	
-	<section class="services">
-		<div class="row">
-			<ul>
-				<li>
-					<div>
-						<img src="<?php echo THEME_IMAGES; ?>temp/category.png" alt="" />
-					</div>
-					<h4>Home Window Cleaning ></h4>
-				</li>
-				<li>
-					<div>
-						<img src="<?php echo THEME_IMAGES; ?>temp/category.png" alt="" />
-					</div>
-					<h4>Home Window Cleaning ></h4>
-				</li>
-				<li>
-					<div>
-						<img src="<?php echo THEME_IMAGES; ?>temp/category.png" alt="" />
-					</div>
-					<h4>Home Window Cleaning ></h4>
-				</li>
-			</ul>
-		</div>
-	</section>
-	
-	<section class="about-us bumper">
-		<div class="row">
-			<article class="span8">
-				<h2>Why Squeeky Clean Window Washing was awarded Best in Bethesda MD.</h2>
-				<p>Yes, Squeeky Clean Window Washing has undergone a rigorous screening process. They have received our top ranking as the best window cleaners in Bethesda Maryland. We are so confident you will be completely satisfied with their window cleaning services that we offer our $1000 Streak Free Guarantee, only through WindowCleaning.com.</p>
-			</article>
-			<aside class="span4">
-				<div class="carousel slide">
-					<div class="carousel-inner">
-						<blockquote class="item">
-							<p>I asked Squeeky Clean to clean my windows and gutters. They did a terrific job on both. The gutters were clogged with dirt and decayed leaves. They cleaned out all the gunk in the gutters. And the windows glowed! Very efficient and client-friendly team. I will use them again next year."</p>
-							<small><cite>Marilou Merrill</cite></small>
-						</blockquote>
-					</div>
-					<ol class="carousel-indicators">
-						<li></li>
-						<li></li>
-					</ol>
+</section>
+
+<section class="services">
+	<div class="row">
+		<ul>
+			<li>
+				<div>
+					<img src="<?php echo THEME_IMAGES; ?>temp/category.png" alt="" />
 				</div>
-			</aside>
-		</div>
-	</section>
+				<h4>Home Window Cleaning ></h4>
+			</li>
+			<li>
+				<div>
+					<img src="<?php echo THEME_IMAGES; ?>temp/category.png" alt="" />
+				</div>
+				<h4>Home Window Cleaning ></h4>
+			</li>
+			<li>
+				<div>
+					<img src="<?php echo THEME_IMAGES; ?>temp/category.png" alt="" />
+				</div>
+				<h4>Home Window Cleaning ></h4>
+			</li>
+		</ul>
+	</div>
+</section>
+
+<section class="about-us bumper">
+	<div class="row">
+		<article class="span8">
+			<h2>Why Squeeky Clean Window Washing was awarded Best in Bethesda MD.</h2>
+			<p>Yes, Squeeky Clean Window Washing has undergone a rigorous screening process. They have received our top ranking as the best window cleaners in Bethesda Maryland. We are so confident you will be completely satisfied with their window cleaning services that we offer our $1000 Streak Free Guarantee, only through WindowCleaning.com.</p>
+		</article>
+		<aside class="span4">
+			<div class="carousel slide">
+				<div class="carousel-inner">
+					<blockquote class="item">
+						<p>I asked Squeeky Clean to clean my windows and gutters. They did a terrific job on both. The gutters were clogged with dirt and decayed leaves. They cleaned out all the gunk in the gutters. And the windows glowed! Very efficient and client-friendly team. I will use them again next year."</p>
+						<small><cite>Marilou Merrill</cite></small>
+					</blockquote>
+				</div>
+				<ol class="carousel-indicators">
+					<li></li>
+					<li></li>
+				</ol>
+			</div>
+		</aside>
+	</div>
+</section>
 	
 	
 	
-</div><!-- / .container -->
 
 
 
