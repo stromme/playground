e7a0c6a7696a60d4ddf77e9efcdeb837e0fe1d70
Josua Leonard <josua.leonard@gmail.com>
2013-03-14 18:41:19 +0700

    Add toolbox footer

---
+++ footer-toolbox.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying the toolbox footer.
+ *
+ * Contains footer content and the closing of the
+ * .container div element.
+ *
+ * @package Hatch
+ * @subpackage
+ * @since
+ */
+?>
+	<!--section class="footer">
+
+	</section-->
+
+</div><!-- / .container -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
