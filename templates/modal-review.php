ffdedf4dc3a32780a7be7d0ff5658130bc685209
Lars <lars@stromme.ca>
2013-03-26 07:23:38 +0545

    Add company field to review modal

--- templates/modal-review.php
+++ templates/modal-review.php
@@ -9,7 +9,7 @@
 ?>
 
 <!-- Review Modal -->
-<div class="modal fade hide" id="new-review">
+<div class="modal hide fade" id="new-review">
 	<div class="modal-header">
 		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 		<h3>So, how did we do?</h3>
@@ -22,17 +22,27 @@
 		<p class="bumper-top-small"> Login with <a href=""><i class="icon-facebook"></i> Facebook</a> or <a href=""><i class="icon-twitter"></i> Twitter</a>, or <a href="">register</a> to add your review.</p>
 		
 		<!-- Show this if the user chooses the register option -->
-		<div class="row-fluid bumper-top-small hide">
-			<div class="span6">
-				<div class="input-prepend">
-				    <span class="add-on"><i class="icon-user"></i></span>
-				    <input class="input-block-level" validation="email" placeholder="Your Name" type="text">
+		<div class="hide">
+			<div class="row-fluid bumper-top-small">
+				<div class="span6">
+					<div class="input-prepend">
+					    <span class="add-on"><i class="icon-user"></i></span>
+					    <input class="input-block-level" placeholder="Your Name" type="text">
+					</div>
+				</div>
+				<div class="span6">
+					<div class="input-prepend">
+					    <span class="add-on"><i class="icon-user"></i></span>
+					    <input class="input-block-level" placeholder="Company" type="text">
+					</div>
 				</div>
 			</div>
-			<div class="span6">
-				<div class="input-prepend">
-				    <span class="add-on"><i class="icon-envelope"></i></span>
-				    <input class="input-block-level" validation="email" placeholder="Email Address" type="text">
+			<div class="row-fluid">
+				<div class="span6">
+					<div class="input-prepend">
+					    <span class="add-on"><i class="icon-envelope"></i></span>
+					    <input class="input-block-level" validation="email" placeholder="Email Address" type="text">
+					</div>
 				</div>
 			</div>
 		</div>
