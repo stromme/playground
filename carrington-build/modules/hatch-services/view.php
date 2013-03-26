802f63c37c5f4e1e9768b54296d85fef87142f60
Josua Leonard <josua.leonard@gmail.com>
2013-03-26 18:26:00 +0700

    Update services module

--- carrington-build/modules/hatch-services/view.php
+++ carrington-build/modules/hatch-services/view.php
@@ -18,20 +18,35 @@
     foreach($services as $service){
       $args = array('post_type' => 'research', 'taxonomy' => 'services', 'term' => $service->slug);
       $is_service_promoted = TB_Promote::is_promoted($args);
-      if($is_service_promoted){
+      $args = array(
+        'post_type'   => 'showroom',
+        'services'    => $service->slug,
+        'order'       => 'ASC',
+        'numberposts' => 1
+      );
+      $service_post_query = new WP_Query( $args );
+      $is_have_post = $service_post_query->have_posts();
   ?>
   <li>
     <div class="grid-thumb">
+      <?php if($is_have_post || $is_service_promoted){ ?>
       <nav>
-        <a href="<?=get_home_url().$blog_prefix."/services/".$service->slug?>"><i class="icon-picture icon-white"></i> Showroom</a>
-        <a href=""><i class="icon-info-sign icon-white"></i> Learn more</a>
+        <?php if($is_have_post){ ?>
+        <a href="<?=get_home_url()."/showroom/".$service->slug?>"><i class="icon-picture icon-white"></i> Showroom</a>
+        <?php } if($is_service_promoted){ ?>
+        <a href="<?=get_home_url().$blog_prefix."/services/".$service->slug?>"><i class="icon-info-sign icon-white"></i> Learn more</a>
+        <?php } ?>
       </nav>
-      <img src="http://localhost/wpmulti/wp-content/themes/playground/images/temp/category.png" alt="">
+      <?php } ?>
+      <img src="<?=get_home_url()?>/wp-content/themes/playground/images/temp/category.png" alt="" />
     </div>
+    <?php if($is_service_promoted){ ?>
     <a href="<?=get_home_url().$blog_prefix."/services/".$service->slug?>"><?=$service->name?></a>
+    <?php } else { ?>
+    <span><?=$service->name?></span>
+    <?php } ?>
   </li>
   <?php
-      }
     }
   ?>
 </ul>
