489154273813132d6cf1e8c5ebae46daead0c7b6
Lars <lars@stromme.ca>
2013-03-26 09:30:44 +0545

    Add reviews page

---
+++ page-templates/page-reviews.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template Name: Reviews Page
+ * Description: 
+ *
+ * @package WordPress
+ * @subpackage Hatch
+ * @since 
+ */
+
+get_header(); 
+
+?>
+
+<!-- Wrap the page in .container to centre the content and keep it at a max width -->
+<div class="container gentle-shadow">
+	
+	<section class="bg-sea margin-left margin-right page-title">
+		<div class="bumper-top-medium bumper-bottom-medium bumper-left-large bumper-right-large center">
+			<h2 class="white">Crystal Clear Window Cleaning <!-- company name -->, loved in your neighborhood.</h2>
+			<div class="pen-stroke-blue"></div>
+		</div>
+	</section>
+	<section class="bg-white page-left page-right bumper-bottom-medium bumper-top-medium">
+		<ul class="reviews-list">
+			<li class="bumper-bottom">
+				<div class="author-callout author-callout-border">
+					<cite>George Stevens</cite> says<b class="author-callout-border-notch notch"></b><b class="notch"></b>
+					<!-- star rating needs to be added to float right in this div -->
+				</div>
+				<p class="bumper-bottom">"My assistant found Ernie of Quality Window Cleaning on the web. He gave us a quote and did a test clean of our windows. We found him to be quick and efficient and he was even able to get rid of some stubborn water stains. Needless to say, we have been very satisfied with his work and his price and would recommend him to other businesses."
+				</p>
+				<div class="pen-stroke"></div>
+			</li>
+			<li class="bumper-bottom">
+				<div class="author-callout author-callout-border bumper-top-medium">
+					<cite>George Stevens</cite> says<b class="author-callout-border-notch notch"></b><b class="notch"></b>
+				</div>
+				<p>"My assistant found Ernie of Quality Window Cleaning on the web. He gave us a quote and did a test clean of our windows. We found him to be quick and efficient and he was even able to get rid of some stubborn water stains. Needless to say, we have been very satisfied with his work and his price and would recommend him to other businesses."
+				</p>
+			</li>
+			
+		</ul>
+		
+		
+	
+	</section>
+	
+
+<?php get_footer(); ?>
