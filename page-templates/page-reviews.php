f88066a564456461e36ee099a227714ed6ebc06f
Josua Leonard <josua.leonard@gmail.com>
2013-04-01 13:12:46 +0700

    Update review page

--- page-templates/page-reviews.php
+++ page-templates/page-reviews.php
@@ -8,8 +8,14 @@
  * @since 
  */
 
-get_header(); 
-
+get_header();
+$company = get_option('tb_company');
+$args = array(
+  'orderby' => 'modified',
+  'order'   => 'DESC',
+  'number'  => 10
+);
+$reviews = get_comments($args);
 ?>
 
 <!-- Wrap the page in .container to centre the content and keep it at a max width -->
@@ -17,29 +23,29 @@ get_header();
 	
 	<section class="bg-sea margin-left margin-right page-title">
 		<div class="bumper-top-medium bumper-bottom-medium bumper-left-large bumper-right-large center">
-			<h2 class="white">Crystal Clear Window Cleaning <!-- company name -->, loved in your neighborhood.</h2>
+			<h2 class="white"><?=$company['name']?>, loved in your neighborhood.</h2>
 			<div class="pen-stroke-blue"></div>
 		</div>
 	</section>
 	<section class="bg-white page-left page-right bumper-bottom-medium bumper-top-medium">
 		<ul class="reviews-list">
+      <?php
+        if(count($reviews)>0){
+          foreach($reviews as $review){
+      ?>
 			<li class="bumper-bottom">
 				<div class="author-callout author-callout-border">
-					<cite>George Stevens</cite> says<b class="author-callout-border-notch notch"></b><b class="notch"></b>
+					<cite><?=$review->comment_author?></cite> says<b class="author-callout-border-notch notch"></b><b class="notch"></b>
 					<!-- star rating needs to be added to float right in this div -->
 				</div>
-				<p class="bumper-bottom">"My assistant found Ernie of Quality Window Cleaning on the web. He gave us a quote and did a test clean of our windows. We found him to be quick and efficient and he was even able to get rid of some stubborn water stains. Needless to say, we have been very satisfied with his work and his price and would recommend him to other businesses."
+				<p class="bumper-bottom">"<?=$review->comment_content?>"
 				</p>
 				<div class="pen-stroke"></div>
 			</li>
-			<li class="bumper-bottom">
-				<div class="author-callout author-callout-border bumper-top-medium">
-					<cite>George Stevens</cite> says<b class="author-callout-border-notch notch"></b><b class="notch"></b>
-				</div>
-				<p>"My assistant found Ernie of Quality Window Cleaning on the web. He gave us a quote and did a test clean of our windows. We found him to be quick and efficient and he was even able to get rid of some stubborn water stains. Needless to say, we have been very satisfied with his work and his price and would recommend him to other businesses."
-				</p>
-			</li>
-			
+      <?php
+          }
+        }
+      ?>
 		</ul>
 		
 		
