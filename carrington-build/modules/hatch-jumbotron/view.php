f51ee94ab55f7793a3d299f785f99639248ef908
Josua Leonard <josua.leonard@gmail.com>
2013-04-04 06:41:06 +0700

    Fix jumbotron width for small text

--- carrington-build/modules/hatch-jumbotron/view.php
+++ carrington-build/modules/hatch-jumbotron/view.php
@@ -1,10 +1,11 @@
 <!-- Hero Module -  JumboTron style - Giant image with a headline text and citation
 ===========================================================================================================
 -->
-
-<div class="jumbotron hidden-phone <?=($border_style=='round')?'round-corner':''?>" style="background: url(<?=$image?>); background-size: cover;">
-	<<?=$heading?> class="white jumbo loud page-left page-right bumper-bottom">"<?=parse_shortclass($title)?>"</<?=$heading?>>
-	<<?=$content_style?> class="white light-weight"><?=parse_shortclass($content)?></<?=$content_style?>>
+<div class="jumbotron">
+  <div class="jumbotron-content hidden-phone <?=($border_style=='round')?'round-corner':''?>" style="background: url(<?=$image?>); background-size: cover;">
+    <<?=$heading?> class="white jumbo loud page-left page-right bumper-bottom">"<?=parse_shortclass($title)?>"</<?=$heading?>>
+    <<?=$content_style?> class="white light-weight"><?=parse_shortclass($content)?></<?=$content_style?>>
+  </div>
 </div>
 
 <!-- / JumboTron -->
