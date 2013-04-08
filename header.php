7a29d98a609a70f456fdcdffa64fe579c2adfcc0
Lars <lars@stromme.ca>
2013-04-08 16:15:06 +0545

    Add style loading to header for child themes

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_head(); ?>
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 </head>
 
