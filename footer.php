8b7e4b3b388c6846abf4c49e2317f6d68ca50fb0
Lars <lars@stromme.ca>
2013-04-08 16:41:26 +0545

    Add generic hatch footer

--- footer.php
+++ footer.php
@@ -11,10 +11,10 @@
  */
 ?>
 	<section class="footer">
-		<img src="<?php echo THEME_IMAGES; ?>brand/window-cleaning-dot-com-logo.png">
+		<img src="<?php echo get_header_image(); ?>">
 		<h3 class="bumper-bottom bumper-top">Like to chat? Call our team anytime at (123) 123-1234 or <a href="">get an online estimate.</a></h3>
-		<p class="footer-links"><a href="">Visit WindowCleaning.com</a><a href="">Our Locations</a><a href="">News About Us</a><a href="">Apply for Membership</a></p>
-		<p><small>North America's Best Window Cleaners | Copyright 2013 | <a href="">Privacy</a> | <a href="">Terms</a></small></p>
+		<p class="footer-links"><a href="">Services</a><a href="">Showroom</a><a href="">Reviews</a></p>
+		<p><small><?php echo get_bloginfo ( 'description' ) ?> | Copyright 2013 </small></p>
 	</section>
 
 </div><!-- / .container -->
