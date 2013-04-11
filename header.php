62fb7f16f32894b076b4f10f00984e7182151f10
Josua Leonard <josua.leonard@gmail.com>
2013-04-11 18:59:47 +0700

    Fix youtube layout for carrington

--- header.php
+++ header.php
@@ -22,13 +22,13 @@
 
 <body>
 <div id="fb-root"></div>
-<!--<script>(function(d, s, id) {-->
-<!--  var js, fjs = d.getElementsByTagName(s)[0];-->
-<!--  if (d.getElementById(id)) return;-->
-<!--  js = d.createElement(s); js.id = id;-->
-<!--  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=270342336310368";-->
-<!--  fjs.parentNode.insertBefore(js, fjs);-->
-<!--}(document, 'script', 'facebook-jssdk'));</script>-->
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=270342336310368";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
 
 <? get_template_part('templates/menu', 'headline'); ?>
