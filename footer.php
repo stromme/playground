69545d489221f28f3f0dc8f924a48f59d6cac151
Lars <lars@stromme.ca>
2013-04-11 19:01:54 +0545

    Update footer phone number

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 ?>
 	<section class="footer">
 		<img src="<?php echo get_header_image(); ?>">
-		<h3 class="bumper-bottom bumper-top">Like to chat? Call our team anytime at (123) 123-1234 or <a href="" class="quick-estimate">get an online estimate.</a></h3>
+		<h3 class="bumper-bottom bumper-top">Like to chat? Call our team anytime at <?=get_phone_number()?> or <a href="" class="quick-estimate">get an online estimate.</a></h3>
 		<p class="footer-links"><a href="<?=home_url()?>/services">Services</a><a href="<?=home_url()?>/showroom">Showroom</a><a href="<?=home_url()?>/reviews">Reviews</a></p>
 		<p><small><?php echo get_bloginfo ( 'description' ) ?> | Copyright 2013 </small></p>
 	</section>
