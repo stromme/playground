eb05a3571683f4c758ae3e84107bc8fddc2c025f
Lars <lars@stromme.ca>
2013-04-15 14:42:24 +0545

    clean up toolbox header and add logo for wc.com industry

--- header-toolbox.php
+++ header-toolbox.php
@@ -11,62 +11,17 @@ if(session_id() == '') {
   session_write_close();
   session_start();
 }
+global $post;
 ?><!DOCTYPE html>
-<!--[if IE 6]>
-<html id="ie6" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 7]>
-<html id="ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html id="ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
-<html <?php language_attributes(); ?>>
-<!--<![endif]-->
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<meta name="apple-mobile-web-app-title" content="Hatch" />
-<link rel="apple-touch-icon" href="<?=get_theme_root_uri().'/'.get_stylesheet()?>/iphone-icon.png"/>
-<title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-	global $page, $paged;
-
-	wp_title( '|', true, 'right' );
-
-	// Add the blog name.
-	bloginfo( 'name' );
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
-
-	?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
-<?php
-	/* We add some JavaScript to pages with the comment form
-	 * to support sites with threaded comments (when in use).
-	 */
-	if ( is_singular() && get_option( 'thread_comments' ) )
-		wp_enqueue_script( 'comment-reply' );
-
-	/* Always have wp_head() just before the closing </head>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to add elements to <head> such
-	 * as styles, scripts, and meta tags.
-	 */
-	wp_head();
-?>
-<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
- 
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta name="apple-mobile-web-app-title" content="Hatch" />
+	<link rel="apple-touch-icon" href="<?=get_theme_root_uri().'/'.get_stylesheet()?>/iphone-icon.png"/>
+	<title>Toolbox | <?=ucwords($post->post_name)?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+	<?php wp_head(); ?>
 </head>
 
 <body>
@@ -88,12 +43,10 @@ if(session_id() == '') {
 <div class="navbar navbar-inverse navbar-fixed-top">
 	<div class="navbar-inner">
     <div class="container-fluid">
-			<!--button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button!-->
-      <a class="brand" href="">Hatch</a>
+    	<a class="brand hidden-phone" href="<?=TOOLBOX_URL?>dashboard">
+    		<?php
+    		$industry = get_option('tb_industry'); 
+    		if ( $industry['industry'] == 'window-cleaning' ) echo '<img src="'. TOOLBOX_IMAGES . '/wc-logo.png">'; else echo '<h2>Hatch</h2>';?></a>
 			<div><!--</div> class="nav-collapse collapse"-->
 				<ul class="nav">
 					<li class="has-nav-icon">
