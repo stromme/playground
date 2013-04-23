d52bc1f3cd6ff5d275b2d7917dfd4f63d7a0cfb0
Josua Leonard <josua.leonard@gmail.com>
2013-04-23 13:15:40 +0700

    Update services transitional

--- templates/list-review.php
+++ templates/list-review.php
@@ -1,8 +1,10 @@
 <?php global $review; ?>
 <li class="bumper-bottom" itemprop="review" itemscope="http://schema.org/Review">
 	<div class="author-callout author-callout-border">
-		<cite itemprop="author"><?=$review->name?></cite> says<b class="author-callout-border-notch notch"></b><b class="notch"></b>
-		<div class="pull-right review-rating" data-score="<?=$review->rating?>"></div>
+    <div class="pull-right review-rating" data-score="<?=$review->rating?>"></div>
+    <div>
+		  <cite itemprop="author"><?=$review->name?></cite> says<b class="author-callout-border-notch notch"></b><b class="notch"></b>
+    </div>
 	</div>
 	<p class="bumper-bottom" itemprop="description">"<?=$review->content?>"
 	</p>
