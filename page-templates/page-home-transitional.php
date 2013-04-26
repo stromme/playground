d0908b73940d9aee6b5e602261bcc64c444622e5
Josua Leonard <josua.leonard@gmail.com>
2013-04-27 01:29:34 +0700

    Remove research post type

--- page-templates/page-home-transitional.php
+++ page-templates/page-home-transitional.php
@@ -832,35 +832,24 @@ foreach($comments as $comment){
         <div>
           <!-- Service area Module - List of locations serviced ===================================================== -->
           <?php
-            $users = get_users();
-            $owner_id = '';
-            $owner_name = '';
-            if(count($users)>0){
-              foreach($users as $user){
-                $roles = $user->roles;
-                if(count($roles)>0){
-                  if($roles[0]=='owner'){
-                    $owner_id = $user->id;
-                    $owner_name = $user->display_name;
-                  }
-                }
-              }
-            }
             $other_blog_locations = array();
             $other_blogs_link = array();
             if($owner_id!='' && $owner_id>0){
               $blogs = get_blogs_of_user($owner_id);
               if(count($blogs)>1){
                 foreach($blogs as $user_blog){
-                  if($user_blog->userblog_id!=get_current_blog_id())
+                  $current_blog_id = get_current_blog_id();
+                  if($user_blog->userblog_id!=$current_blog_id){
                     switch_to_blog($user_blog->userblog_id);
+                  }
                   $blog_seo = get_option('tb_seo');
-                  if($user_blog->userblog_id!=get_current_blog_id())
+                  if($user_blog->userblog_id!=$current_blog_id){
                     restore_current_blog();
+                  }
                   if(isset($blog_seo)){
                     if(isset($blog_seo['seo_target_city']) && isset($blog_seo['seo_target_state'])){
                       $blog_seo_slug = strtolower(preg_replace("/[^A-Za-z0-9\_\-]/", '', $blog_seo['seo_target_city'])).'-'.strtolower($blog_seo['seo_target_state']);
-                      $args = array('post_type' => 'research', 'taxonomy' => 'locations', 'term' => $blog_seo_slug);
+                      $args = array('post_type' => 'cftl-tax-landing', 'taxonomy' => 'locations', 'term' => $blog_seo_slug);
                       $is_location_promoted = TB_Promote::is_promoted($args);
                       $blog_seo_promoted_url = '';
                       if($is_location_promoted) $blog_seo_promoted_url = home_url().get_blog_prefix().'locations/'.$blog_seo_slug;
@@ -889,7 +878,7 @@ foreach($comments as $comment){
                 if(count($locations)>0){
                   foreach ($locations as $location) {
                     $name = $location->name;
-                    $args = array('post_type' => 'research', 'taxonomy' => 'locations', 'term' => $location->slug);
+                    $args = array('post_type' => 'cftl-tax-landing', 'taxonomy' => 'locations', 'term' => $location->slug);
                     $is_location_promoted = TB_Promote::is_promoted($args);
                     if($is_location_promoted){
                       $name = '<a href="'.home_url().((get_blog_prefix()!='')?get_blog_prefix():'/').'locations/'.($location->slug).'">'.$name.'</a>';
