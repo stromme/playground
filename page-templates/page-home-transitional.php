fc068ee95bc5a6fda7e0d7efc0896dd509c33e5c
Lars <lars@stromme.ca>
2013-05-02 07:37:17 +0545

    Proud members title

--- page-templates/page-home-transitional.php
+++ page-templates/page-home-transitional.php
@@ -830,9 +830,12 @@ if($accolades_post && count($accolades_post)>0){
     $id = 'home_transitional_certifications';
   ?>
   <!-- Certifications -->
-  <section class="bg-white bumper-top-medium bumper-bottom-small page-left page-right">
+  <section class="bg-white bumper-top bumper-bottom-small page-left page-right">
     <div id="<?=$id?>" class="<?=(count($transitional_certifications)>1)?'carousel-'.$id:''?> accolade-carousel slide">
       <?php if(count($transitional_certifications)>1){ ?>
+      <div class="center">
+      	<h2>We're <strong class="green">proud to be members</strong> of the:</h2>
+      </div>
       <div class="carousel-inner">
       <?php } ?>
         <?php
