a0214f1e11c8d35a7a9aa836f4c47bcce4ffdd8f
Lars <lars@stromme.ca>
2013-05-03 09:08:03 +0545

    Fix link on 404 page

--- 404.php
+++ 404.php
@@ -12,7 +12,7 @@ get_header(); ?>
 	<section class="bg-white gentle-shadow">
 		<div class="center page-left page-right bumper-top-large bumper-bottom-large">
 			<h2>Hmmm...Well this page used to be here.</h2>
-			<h3 class="bumper-top-large">Barring any miraculous re-appearance of what was sure to have been a tremendous viewing experience, it may be best that you <a href="www.hatchspot.com">click this link</a> and visit a page that still exists.</h3>
+			<h3 class="bumper-top-large">Barring any miraculous re-appearance of what was sure to have been a tremendous viewing experience, it may be best that you <a href="<?=get_site_url(1)?>">click this link</a> and visit a page that still exists.</h3>
 		</div>
 	</section>
 
