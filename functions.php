8ab37e490934dccf43f08ef7f8a7f3f7c45e62cb
Josua Leonard <josua.leonard@gmail.com>
2013-05-06 14:39:05 +0700

    Restore functions.php

--- functions.php
+++ functions.php
@@ -13,15 +13,6 @@
  * @since 0.0.1 
  */
 
-/**
- * Fix redirect for non www address
- */
-function redirect_non_www(){
-  var_dump($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-  exit;
-}
-redirect_non_www();
-
 /* Define the environment we're working in. DEV or LIVE. */
 define( 'ENVIRONMENT', 'DEV' );
 define( 'TOOLBOX_BASE_DIR', trailingslashit( get_template_directory() ) . 'toolbox-framework' );
