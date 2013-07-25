d7d9ad7615710862b0597afc9f4fad16ad9ccd4a
Josua Leonard <josua.leonard@gmail.com>
2013-07-25 12:48:18 +0700

    Update whitelist

--- functions.php
+++ functions.php
@@ -12,8 +12,7 @@
  * @since 0.1.0 
  */
 
-//$whitelist = array('127.0.0.1', '::1');
-$whitelist = array();
+$whitelist = array('127.0.0.1', '::1');
 if ( !in_array($_SERVER['REMOTE_ADDR'], $whitelist) ) {
    define( 'ENVIRONMENT', 'LIVE' );
 } else {
