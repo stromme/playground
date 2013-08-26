412491ca5225b34ad1bd8a021456e871d58fea68
Josua Leonard <josua.leonard@gmail.com>
2013-08-26 11:47:17 +0700

    Fix index and 404

--- 404.php
+++ 404.php
@@ -7,12 +7,13 @@
  * @since 
  */
 
-get_header(); ?>
+get_header();
+?>
 
 	<section class="bg-white gentle-shadow">
 		<div class="center page-left page-right bumper-top-large bumper-bottom-large">
 			<h2>Sorry...Looks like this page doesn't exist.</h2>
-			<h3 class="bumper-top-large"><a href="<?=get_site_url(1)?>">Click here </a> to continue to our site.</h3>
+			<h3 class="bumper-top-large"><a href="<?=get_site_url()?>">Click here </a> to continue to our site.</h3>
 		</div>
 	</section>
 
