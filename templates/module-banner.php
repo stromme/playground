cddb6363efb9ca03039bcddd8b5c73f938a52d5c
Josua Leonard <josua.leonard@gmail.com>
2013-12-17 15:36:46 +0700

    Update twitter share button

--- templates/module-banner.php
+++ templates/module-banner.php
@@ -23,7 +23,8 @@
 				<li class="write-review"><a href=""><i class="icon-full-conversation"></i><span> Write your review</span></a></li>
 				<!-- Commented out for local development -->
 				<li class="social-network"><div class="fb-like" data-send="false" data-layout="button_count" data-width="100" data-show-faces="false" data-font="verdana" data-action="like"></div></li>
-				<li class="social-network"><a href="https://twitter.com/share" class="twitter-share-button" data-via="streakfreeclean" data-related="streakfreeclean" data-hashtags="WindowCleaning">Tweet</a>
+        <?php $tw = get_site_option('tb_twitter_share'); ?>
+				<li class="social-network"><a href="https://twitter.com/share" class="twitter-share-button" <?=(isset($tw['account'])?'data-via="'.$tw['account'].'" data-related="'.$tw['account'].'"':'')?> <?=(isset($tw['hashtags'])?'data-hashtags="'.$tw['hashtags'].'"':'')?>>Tweet</a>
 				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></li>
 			</ul>
 		</div>
