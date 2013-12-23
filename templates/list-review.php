140e08f06d48cf57d119abd9f32a205819c66e71
Josua Leonard <josua.leonard@gmail.com>
2013-12-23 09:45:38 +0700

    Update microdata

--- templates/list-review.php
+++ templates/list-review.php
@@ -2,11 +2,12 @@
 <li class="bumper-bottom" itemprop="review" itemscope="http://schema.org/Review">
 	<div class="author-callout author-border-callout">
     <div class="pull-right review-rating" data-score="<?=$review->rating?>"></div>
+    <span class="reviewRating"><?=$review->rating?></span>
     <div>
 		  <cite itemprop="author"><?=$review->name?></cite> <?=get_comment_says($review->name)?><b class="author-border-notch notch"></b><b class="notch"></b>
     </div>
 	</div>
-	<p class="bumper-bottom" itemprop="description">"<?=$review->content?>"
+	<p class="bumper-bottom">"<span itemprop="reviewBody"><?=$review->content?></span>"
 	</p>
 	<div class="pen-stroke"></div>
 </li>
