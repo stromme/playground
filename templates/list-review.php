c979a649bd95badc2dfe977c6b1ad7338ad6da77
Josua Leonard <josua.leonard@gmail.com>
2013-12-26 12:11:14 +0700

    fix review rating

--- templates/list-review.php
+++ templates/list-review.php
@@ -2,7 +2,7 @@
 <li class="bumper-bottom" itemprop="review" itemscope="http://schema.org/Review">
 	<div class="author-callout author-border-callout">
     <div class="pull-right review-rating" data-score="<?=$review->rating?>"></div>
-    <span class="reviewRating"><?=$review->rating?></span>
+    <span class="hide" itemprop="reviewRating"><?=$review->rating?></span>
     <div>
 		  <cite itemprop="author"><?=$review->name?></cite> <?=get_comment_says($review->name)?><b class="author-border-notch notch"></b><b class="notch"></b>
     </div>
