d75dd3abcef207377b94d0a85fee57315586fa12
Josua Leonard <josua.leonard@gmail.com>
2014-04-25 13:38:56 +0700

    Update year

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@ restore_current_blog();
 		<img src="<?php echo get_header_image(); ?>">
 		<h3 class="bumper-bottom bumper-top">Like to chat? Call our team anytime at <?=get_phone_number()?> or <a href="javascript:void(0);" class="quick-estimate <?=apply_filters('apply_responsibid', false);?>">get an online estimate.</a></h3>
 		<p class="footer-links"><a href="<?=home_url()?>/services">Services</a><a href="<?=home_url()?>/showroom">Showroom</a><a href="<?=home_url()?>/reviews">Reviews</a></p>
-		<p><small><?php echo get_bloginfo ( 'description' ) ?> | Copyright 2013 </small></p>
+		<p><small><?php echo get_bloginfo ( 'description' ) ?> | Copyright <?=date('Y')?> </small></p>
 	</section>
 
 </div><!-- / .container -->
