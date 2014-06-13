67e42425b9799359ad367ae5f655e2d095959839
Josua Leonard <josua.leonard@gmail.com>
2014-06-13 12:37:59 +0700

    Update ricch text

--- carrington-build/modules/rich-text/rich-text.php
+++ carrington-build/modules/rich-text/rich-text.php
@@ -31,7 +31,7 @@ if (!class_exists('cfct_module_rich_text')) {
 			$content = (isset($data[$this->get_field_name('content')]) ? $data[$this->get_field_name('content')] : '');
 			ob_start();
 			wp_editor($content, $this->get_field_id('content'), array(
-				'media_buttons' => false,
+				'media_buttons' => true,
 				'textarea_name' => $this->get_field_name('content'),
 			));
 			$ret = ob_get_clean();
@@ -74,7 +74,14 @@ if (!class_exists('cfct_module_rich_text')) {
 					background-color: #fff;
 				}
 				#cfct-rich-text-edit-form .wp-editor-tabs {
-					display: none;
+					//display: none;
+				}
+				#cfct-rich-text-edit-form .wp-media-buttons .button {
+					margin: 2px 0 0 2px;
+				}
+				#cfct-rich-text-edit-form .cfct-popup-content {
+					overflow: visible !important;
+					max-height: none !important;
 				}
 			';
 		}
@@ -85,8 +92,51 @@ if (!class_exists('cfct_module_rich_text')) {
 				cfct_builder.addModuleLoadCallback("'.$this->id_base.'",function(form) {
 					tinymce.init({
 						selector: "#'.$this->get_field_id('content').'",
-						menubar: false
+						menubar: false,
+            theme:"modern",
+            skin:"lightgray",
+            language:"en",
+            formats:{
+              alignleft: [
+                {selector: "p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li", styles: {textAlign:"left"}},
+                {selector: "img,table,dl.wp-caption", classes: "alignleft"}
+              ],
+              aligncenter: [
+                {selector: "p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li", styles: {textAlign:"center"}},
+                {selector: "img,table,dl.wp-caption", classes: "aligncenter"}
+              ],
+              alignright: [
+                {selector: "p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li", styles: {textAlign:"right"}},
+                {selector: "img,table,dl.wp-caption", classes: "alignright"}
+              ],
+              strikethrough: {inline: "del"}
+            },
+            relative_urls:false,
+            remove_script_host:false,
+            convert_urls:false,
+            browser_spellcheck:true,
+            fix_list_elements:true,
+            entities:"38,amp,60,lt,62,gt",
+            entity_encoding:"raw",
+            keep_styles:false,paste_webkit_styles:"font-weight font-style color",
+            preview_styles:"font-family font-size font-weight font-style text-decoration text-transform",
+            wpeditimage_disable_captions:false,
+            wpeditimage_html5_captions:false,
+            plugins:"charmap,hr,media,paste,tabfocus,textcolor,fullscreen,wordpress,wpeditimage,wpgallery,wplink,wpdialogs,wpview,wpfullscreen",
+            content_css:"'.get_home_url().'/wp-includes/css/dashicons.min.css?ver=3.9.1,'.get_home_url().'/wp-includes/js/mediaelement/mediaelementplayer.min.css?ver=3.9.1,'.get_home_url().'/wp-includes/js/mediaelement/wp-mediaelement.css?ver=3.9.1,'.get_home_url().'/wp-includes/js/tinymce/skins/wordpress/wp-content.css?ver=3.9.1",
+            wpautop:true,indent:false,
+            toolbar1:"bold,italic,strikethrough,bullist,numlist,blockquote,hr,alignleft,aligncenter,alignright,link,unlink,spellchecker,wp_fullscreen,wp_adv,code",
+            toolbar2:"formatselect,underline,alignjustify,forecolor,pastetext,removeformat,charmap,outdent,indent,undo,redo",
+            toolbar3:"",toolbar4:"",
+            tabfocus_elements:"cfct-rich-text-content-tmce",
+            body_class:"content post-type-page post-status-auto-draft",add_unload_trigger:false
 					});
+					try {
+            quicktags({
+              id:"'.$this->get_field_id('content').'",
+              buttons:"strong,em,link,block,del,ins,img,ul,ol,li,code,more,close,fullscreen"
+            });
+          } catch(e){};
 					setTimeout(function() {tinyMCE.execCommand("mceFocus", true, "'.$this->get_field_id('content').'");}, 10);
 
 					// properly destroy the editor on cancel
@@ -112,10 +162,11 @@ if (!class_exists('cfct_module_rich_text')) {
         $set = _WP_Editors::parse_settings($this->get_field_id('content'), array(
           'dfw' => true,
           'editor_height' => 300,
+          'editor_css' => true,
           'tinymce' => array(
             'resize' => false,
             'add_unload_trigger' => false,
-          ),
+          )
         ));
         _WP_Editors::editor_settings($this->get_field_id('content'), $set);
       }
