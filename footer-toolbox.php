82c71526050a95d950876265bd1de742a0e95dc1
Josua Leonard <josua.leonard@gmail.com>
2014-07-22 10:46:59 +0700

    lock toolbox

--- footer-toolbox.php
+++ footer-toolbox.php
@@ -13,7 +13,35 @@
 	<!--section class="footer">
 
 	</section-->
-
+<div class="modal fade in" id="toolbox-lock" aria-hidden="false">
+  <div class="modal-body">
+    <p>We're doing server upgrade right now. This will be scheduled to be done not longer than 24 hours or faster. Please be patient and check a gain in a few hours.</p>
+    <p>Thank you.</p>
+  </div>
+</div>
+<div class="toolbox-lock"></div>
+<style>
+  #toolbox-lock {
+    display: block;
+    top: 20%;
+    position: fixed;
+    margin-top: 0;
+    z-index: 1029;
+  }
+  .toolbox-lock {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 1028;
+    background-color: #000;
+    opacity: 0.8;
+  }
+  .action-new-project {
+    display: none;
+  }
+</style>
 </div><!-- / .container -->
 
 <?php wp_footer(); ?>
