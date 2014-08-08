91759403510bfb2fadb873df2043524c216c4704
Josua Leonard <josua.leonard@gmail.com>
2014-08-09 01:13:49 +0700

    Fix location map

--- carrington-build/modules/hatch-address/view.php
+++ carrington-build/modules/hatch-address/view.php
@@ -6,8 +6,11 @@
 $company_name = stripslashes(isset($company['name'])?$company['name']:'');
 $company_street = isset($company['street'])?$company['street']:'';
 $company_country = isset($company['country'])?$company['country']:'';
+$company_country_name = tb_get_countries($company_country);
 $company_city = isset($company['city'])?$company['city']:'';
 $company_state = isset($company['state'])?$company['state']:'';
+$country_states = tb_get_states($company_country);
+$company_state_name = (isset($country_states[$company_state]))?$country_states[$company_state]:'';
 $company_zip = isset($company['zip'])?$company['zip']:'';
 $company_phone = tb_format_us_phone_number(isset($company['phone'])?$company['phone']:'', false);
 $users = get_users();
@@ -32,7 +35,7 @@ if(count($users)>0){
 
 <div class="row-fluid">
   <div class="mobile12 address-map pull-left bumper-right-medium bumper-bottom">
-    <img class="img-polaroid" src="http://maps.google.com/maps/api/staticmap?center=<?=urlencode($company_street).'+'.urlencode($company_city.', '.$company_state).'+'.urlencode($company_zip)?>&amp;zoom=11&amp;size=270x210&amp;maptype=roadmap&amp;markers=color:blue%7Clabel:A%7C<?=urlencode($company_street).'+'.urlencode($company_city.', '.$company_state).'+'.urlencode($company_zip)?>&amp;sensor=false&amp;key=AIzaSyDsgeBIjdh92uqzr0jWMHz_2YRljj_4sxc">
+    <img class="img-polaroid" src="http://maps.google.com/maps/api/staticmap?center=<?=urlencode($company_street).'+'.urlencode($company_city.','.$company_state_name).','.$company_country_name?>&amp;zoom=11&amp;size=270x210&amp;maptype=roadmap&amp;markers=size:mid%7Ccolor:red%7Clabel:H%7C<?=urlencode($company_street).'+'.urlencode($company_city.','.$company_state_name).'+'.urlencode($company_zip).','.$company_country_name?>&amp;key=AIzaSyDsgeBIjdh92uqzr0jWMHz_2YRljj_4sxc" />
   </div>
   <div class="mobile12 address">
     <h3 class="light-weight"><?=parse_shortclass($title)?></h3>
@@ -40,7 +43,7 @@ if(count($users)>0){
       <strong><?=$company_name?></strong><br>
       <span itemprop="streetAddress"><?=$company_street?></span><br />
       <span itemprop="addressLocality"><?=$company_city?></span>, <?=$company_state?> <?=$company_zip?><br />
-      <span class="hide" itemprop="addressCountry"><?=$company_country?></span><?php if($company_country!='' && $company_country!='US'){ ?><?=tb_get_countries($company_country)?><br /><?php } ?>
+      <span class="hide" itemprop="addressCountry"><?=$company_country?></span><?php if($company_country!='' && $company_country!='US'){ ?><?=$company_country_name?><br /><?php } ?>
       <?php if($company_phone!=''){ ?><abbr title="Phone">P:</abbr> <span itemprop="telephone"><?=$company_phone?></span><?php } ?>
     </address>
 
